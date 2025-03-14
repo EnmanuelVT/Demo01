21a99e22ada384ef7439142c1eafac0a20aaaa92
Emmanuel <evillavizartrinidad@gmail.com>
2025-03-14 19:04:22 -0400

    Demo 14 y 15.

---
+++ Demo15WebAppExample/Demo.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Demo.aspx.cs" Inherits="Demo15WebAppExample.Demo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Image ID="imgFoto" runat="server" Height="350px" Width="320px" />
+        <asp:Button ID="btnGuardar" runat="server" OnClick="btnGuardar_Click" Text="Guardar" />
+        <div>
+            <asp:FileUpload ID="fuArchivo" runat="server" AllowMultiple="True" />
+        </div>
+        <p>
+            <asp:TextBox ID="txtSalida" runat="server" Height="66px" TextMode="MultiLine" Width="283px"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Demo15WebAppExample/Demo.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Amazon;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using System.IO;
+
+
+namespace Demo15WebAppExample
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        protected void ShowMessage(string message)
+        {
+            ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + message + "');", true);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (fuArchivo.HasFiles)
+            {
+                foreach (var item in fuArchivo.PostedFiles)
+                {
+                    item.SaveAs(MapPath("Imagen") + "\\" + item.FileName);
+                }
+            }
+            if (fuArchivo.HasFile)
+            {
+                fuArchivo.SaveAs(MapPath("Imagen") + "\\" + fuArchivo.FileName);
+
+                AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient("PONTULLAVE", "PONTULLAVE", RegionEndpoint.USEast1);
+                Amazon.Rekognition.Model.Image image = new Amazon.Rekognition.Model.Image();
+
+                try
+                {
+                    using (FileStream fs = new FileStream(fuArchivo.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        byte[] data = null;
+                        data = new byte[fs.Length];
+                        fs.Read(data, 0, (int)fs.Length);
+                        image.Bytes = new MemoryStream(data);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ShowMessage(ex.Message);
+                    throw;
+                }
+
+
+                DetectModerationLabelsRequest requestMod = new DetectModerationLabelsRequest() { Image = image };
+                DetectModerationLabelsResponse responseMod = rekognitionClient.DetectModerationLabels(requestMod);
+
+                var responseMessageMod = "";
+
+                foreach (var item in responseMod.ModerationLabels)
+                {
+                    responseMessageMod += item.Name.ToString() + "=" + item.Confidence.ToString() + "\r\n";
+                }
+
+
+            }
+            imgFoto.ImageUrl = "/imagen/" + fuArchivo.FileName;
+        }
+    }
+}
